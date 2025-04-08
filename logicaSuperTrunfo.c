481cee46c004f56669bed28dad288d0192c6c708
Jeferson Oliveira <zuruckyt@gmail.com>
2025-04-07 22:02:08 -0300

    feat: beginner

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,90 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+struct Card {
+    char state[3];
+    char code[3];
+    char name[21];
+    int population;
+    float area;
+    float gdp;
+    int poi;
+    float populationDensity;
+    float gdpPerCapita;
+    float superPower;
+};
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    struct Card cardOne;
+    struct Card cardTwo;
 
+    printf("Cadastro de Cartas\n");
+
+    printf("=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~==~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=\n");
+
+    printf("Digite o Estado da Primeira Carta: ");
+    scanf(" %2s", cardOne.state);
+
+    printf("Digite o Código da Primeira Carta: ");
+    scanf(" %2s", cardOne.code);
+
+    printf("Digite o Nome da Primeira Carta: ");
+    scanf(" %20s", cardOne.name);
+
+    printf("Digite a População da Primeira Carta: ");
+    scanf(" %d", &cardOne.population);
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    printf("Digite a Área da Primeira Carta: ");
+    scanf(" %f", &cardOne.area);
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("Digite o PIB da Primeira Carta: ");
+    scanf(" %f", &cardOne.gdp);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    printf("Digite o Número de Pontos Turísticos da Primeira Carta: ");
+    scanf(" %d", &cardOne.poi);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    printf("=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~==~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=\n");
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    printf("Digite o Estado da Segunda Carta: ");
+    scanf(" %2s", cardTwo.state);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    printf("Digite o Código da Segunda Carta: ");
+    scanf(" %2s", cardTwo.code);
 
+    printf("Digite o Nome da Segunda Carta: ");
+    scanf(" %20s", cardTwo.name);
+
+    printf("Digite a População da Segunda Carta: ");
+    scanf(" %d", &cardTwo.population);
+    
+    printf("Digite a Área da Segunda Carta: ");
+    scanf(" %f", &cardTwo.area);
+
+    printf("Digite o PIB da Segunda Carta: ");
+    scanf(" %f", &cardTwo.gdp);
+
+    printf("Digite o Número de Pontos Turísticos da Segunda Carta: ");
+    scanf(" %d", &cardTwo.poi);
+
+    cardOne.populationDensity = cardOne.population / cardOne.area;
+    cardTwo.populationDensity = cardTwo.population / cardTwo.area;
+
+    cardOne.gdpPerCapita = cardOne.gdp / cardOne.population;
+    cardTwo.gdpPerCapita = cardTwo.gdp / cardTwo.population;
+
+    cardOne.superPower = (float) cardOne.population + (float) cardOne.poi + cardOne.area + (1 / cardOne.populationDensity) + cardOne.gdp, cardOne.gdpPerCapita;
+    cardTwo.superPower = (float) cardTwo.population + (float) cardTwo.poi + cardTwo.area + (1 / cardTwo.populationDensity) + cardTwo.gdp, cardTwo.gdpPerCapita;
+    printf("=~=~=~=~=~=~=~=~=~=~=Comparação de Cartas=~=~=~=~=~=~=~=~=~=~=\n");
+    
+    printf("Carta 1 - %s (%s): %d\n", cardOne.name, cardOne.state, cardOne.population);
+    printf("Carta 2 - %s (%s): %d\n", cardTwo.name, cardTwo.state, cardTwo.population);
+
+    if (cardOne.population > cardTwo.population) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
+    } else if (cardTwo.population > cardOne.population) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
+    } else {
+        printf("Empate\n");
+    }
+    
     return 0;
 }
