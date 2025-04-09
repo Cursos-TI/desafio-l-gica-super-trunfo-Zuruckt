839bd0688917694f1fb8ae3b9d6c56250018cb89
Jeferson Oliveira <zuruckyt@gmail.com>
2025-04-08 23:01:40 -0300

    feat: master

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-struct Card {
+struct Card
+{
     char state[3];
     char code[3];
     char name[21];
@@ -11,13 +12,15 @@ struct Card {
     float populationDensity;
     float gdpPerCapita;
     float superPower;
+    float decisionsSum;
 };
 
-int main() {
+int main()
+{
     struct Card cardOne;
     struct Card cardTwo;
 
-    int decision;
+    int decision[2];
 
     printf("Cadastro de Cartas\n");
 
@@ -34,7 +37,7 @@ int main() {
 
     printf("Digite a População da Primeira Carta: ");
     scanf(" %d", &cardOne.population);
-    
+
     printf("Digite a Área da Primeira Carta: ");
     scanf(" %f", &cardOne.area);
 
@@ -57,7 +60,7 @@ int main() {
 
     printf("Digite a População da Segunda Carta: ");
     scanf(" %d", &cardTwo.population);
-    
+
     printf("Digite a Área da Segunda Carta: ");
     scanf(" %f", &cardTwo.area);
 
@@ -73,84 +76,160 @@ int main() {
     cardOne.gdpPerCapita = cardOne.gdp / cardOne.population;
     cardTwo.gdpPerCapita = cardTwo.gdp / cardTwo.population;
 
-    cardOne.superPower = (float) cardOne.population + (float) cardOne.poi + cardOne.area + (1 / cardOne.populationDensity) + cardOne.gdp, cardOne.gdpPerCapita;
-    cardTwo.superPower = (float) cardTwo.population + (float) cardTwo.poi + cardTwo.area + (1 / cardTwo.populationDensity) + cardTwo.gdp, cardTwo.gdpPerCapita;
+    cardOne.superPower = (float)cardOne.population + (float)cardOne.poi + cardOne.area + (1 / cardOne.populationDensity) + cardOne.gdp, cardOne.gdpPerCapita;
+    cardTwo.superPower = (float)cardTwo.population + (float)cardTwo.poi + cardTwo.area + (1 / cardTwo.populationDensity) + cardTwo.gdp, cardTwo.gdpPerCapita;
     printf("=~=~=~=~=~=~=~=~=~=~=Comparação de Cartas=~=~=~=~=~=~=~=~=~=~=\n");
 
-    printf("Escolha o critério de comparação:\n");
-    printf("1 - População\n");
-    printf("2 - Área\n");
-    printf("3 - PIB\n");
-    printf("4 - Número de Pontos Turísticos\n");
-    printf("5 - Densidade Populacional\n");
-
-    scanf(" %d", &decision);
-
-    switch (decision)
+    for (int i = 0; i <= 1; i++)
     {
-    case 1:
-        printf("Comparando por População\n");
-        printf("Carta 1 - %s (%s): %d\n", cardOne.name, cardOne.state, cardOne.population);
-        printf("Carta 2 - %s (%s): %d\n", cardTwo.name, cardTwo.state, cardTwo.population);
-        if (cardOne.population > cardTwo.population) {
-            printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
-        } else if (cardTwo.population > cardOne.population) {
-            printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
-        } else {
-            printf("Empate\n");
+        printf("Escolha %d o critério de comparação:\n", i + 1);
+        if (decision[0] != 1)
+        {
+            printf("1 - População\n");
         }
-        break;
-    case 2:
-        printf("Comparando por Área\n");
-        printf("Carta 1 - %s (%s): %.2f\n", cardOne.name, cardOne.state, cardOne.area);
-        printf("Carta 2 - %s (%s): %.2f\n", cardTwo.name, cardTwo.state, cardTwo.area);
-        if (cardOne.area > cardTwo.area) {
-            printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
-        } else if (cardTwo.area > cardOne.area) {
-            printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
-        } else {
-            printf("Empate\n");
+        if (decision[0] != 2)
+        {
+            printf("2 - Área\n");
         }
-        break;
-    case 3:
-        printf("Comparando por PIB\n");
-        printf("Carta 1 - %s (%s): %.2f\n", cardOne.name, cardOne.state, cardOne.gdp);
-        printf("Carta 2 - %s (%s): %.2f\n", cardTwo.name, cardTwo.state, cardTwo.gdp);
-        if (cardOne.gdp > cardTwo.gdp) {
-            printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
-        } else if (cardTwo.gdp > cardOne.gdp) {
-            printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
-        } else {
-            printf("Empate\n");
+        if (decision[0] != 3)
+        {
+            printf("3 - PIB\n");
         }
-        break;
-    case 4:
-        printf("Comparando por Número de Pontos Turísticos\n");
-        printf("Carta 1 - %s (%s): %d\n", cardOne.name, cardOne.state, cardOne.poi);
-        printf("Carta 2 - %s (%s): %d\n", cardTwo.name, cardTwo.state, cardTwo.poi);
-        if (cardOne.poi > cardTwo.poi) {
-            printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
-        } else if (cardTwo.poi > cardOne.poi) {
-            printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
-        } else {
-            printf("Empate\n");
+        if (decision[0] != 4)
+        {
+            printf("4 - Número de Pontos Turísticos\n");
         }
-        break;
-    case 5:
-        printf("Comparando por Densidade Populacional\n");
-        printf("Carta 1 - %s (%s): %.2f\n", cardOne.name, cardOne.state, cardOne.populationDensity);
-        printf("Carta 2 - %s (%s): %.2f\n", cardTwo.name, cardTwo.state, cardTwo.populationDensity);
-        if (cardOne.populationDensity < cardTwo.populationDensity) {
-            printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
-        } else if (cardTwo.populationDensity < cardOne.populationDensity) {
-            printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
-        } else {
-            printf("Empate\n");
+        if (decision[0] != 5)
+        {
+            printf("5 - Densidade Populacional\n");
         }
-        break;
-    default:
-        printf("Opção inválida\n");
-        break;
+
+        scanf(" %d", &decision[i]);
+    }
+
+    printf("\n");
+    printf("=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=");
+    printf("\n");
+
+    for (int i = 0; i < 2; i++)
+    {
+        switch (decision[i])
+        {
+        case 1:
+            cardOne.decisionsSum = cardOne.decisionsSum + cardOne.population;
+            cardTwo.decisionsSum = cardTwo.decisionsSum + cardTwo.population;
+            printf("Comparando por População\n");
+            printf("Carta 1 - %s (%s): %d\n", cardOne.name, cardOne.state, cardOne.population);
+            printf("Carta 2 - %s (%s): %d\n", cardTwo.name, cardTwo.state, cardTwo.population);
+            if (cardOne.population > cardTwo.population)
+            {
+                printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
+            }
+            else if (cardTwo.population > cardOne.population)
+            {
+                printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
+            }
+            else
+            {
+                printf("Empate\n");
+            }
+            break;
+        case 2:
+            cardOne.decisionsSum = cardOne.decisionsSum + cardOne.area;
+            cardTwo.decisionsSum = cardTwo.decisionsSum + cardTwo.area;
+            printf("Comparando por Área\n");
+            printf("Carta 1 - %s (%s): %.2f\n", cardOne.name, cardOne.state, cardOne.area);
+            printf("Carta 2 - %s (%s): %.2f\n", cardTwo.name, cardTwo.state, cardTwo.area);
+            if (cardOne.area > cardTwo.area)
+            {
+                printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
+            }
+            else if (cardTwo.area > cardOne.area)
+            {
+                printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
+            }
+            else
+            {
+                printf("Empate\n");
+            }
+            break;
+        case 3:
+            cardOne.decisionsSum = cardOne.decisionsSum + cardOne.gdp;
+            cardTwo.decisionsSum = cardTwo.decisionsSum + cardTwo.gdp;
+            printf("Comparando por PIB\n");
+            printf("Carta 1 - %s (%s): %.2f\n", cardOne.name, cardOne.state, cardOne.gdp);
+            printf("Carta 2 - %s (%s): %.2f\n", cardTwo.name, cardTwo.state, cardTwo.gdp);
+            if (cardOne.gdp > cardTwo.gdp)
+            {
+                printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
+            }
+            else if (cardTwo.gdp > cardOne.gdp)
+            {
+                printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
+            }
+            else
+            {
+                printf("Empate\n");
+            }
+            break;
+        case 4:
+            cardOne.decisionsSum = cardOne.decisionsSum + cardOne.poi;
+            cardTwo.decisionsSum = cardTwo.decisionsSum + cardTwo.poi;
+            printf("Comparando por Número de Pontos Turísticos\n");
+            printf("Carta 1 - %s (%s): %d\n", cardOne.name, cardOne.state, cardOne.poi);
+            printf("Carta 2 - %s (%s): %d\n", cardTwo.name, cardTwo.state, cardTwo.poi);
+            if (cardOne.poi > cardTwo.poi)
+            {
+                printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
+            }
+            else if (cardTwo.poi > cardOne.poi)
+            {
+                printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
+            }
+            else
+            {
+                printf("Empate\n");
+            }
+            break;
+        case 5:
+            cardOne.decisionsSum = cardOne.decisionsSum + cardOne.populationDensity;
+            cardTwo.decisionsSum = cardTwo.decisionsSum + cardTwo.populationDensity;
+            printf("Comparando por Densidade Populacional\n");
+            printf("Carta 1 - %s (%s): %.2f\n", cardOne.name, cardOne.state, cardOne.populationDensity);
+            printf("Carta 2 - %s (%s): %.2f\n", cardTwo.name, cardTwo.state, cardTwo.populationDensity);
+            if (cardOne.populationDensity < cardTwo.populationDensity)
+            {
+                printf("Resultado: Carta 1 (%s) venceu!\n", cardOne.name);
+            }
+            else if (cardTwo.populationDensity < cardOne.populationDensity)
+            {
+                printf("Resultado: Carta 2 (%s) venceu!\n", cardTwo.name);
+            }
+            else
+            {
+                printf("Empate\n");
+            }
+            break;
+        default:
+            printf("Opção inválida\n");
+            break;
+        }
+    }
+
+    printf("\n");
+    printf("=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=");
+    printf("\n");
+
+    if (cardOne.decisionsSum > cardTwo.decisionsSum) {
+        printf("Resultado: Carta 1 (%s) venceu o resultado geral!\n", cardOne.name);
+    }
+
+    if (cardOne.decisionsSum < cardTwo.decisionsSum) {
+        printf("Resultado: Carta 2 (%s) venceu o resultado geral!\n", cardTwo.name);
+    }
+
+    if (cardOne.decisionsSum == cardTwo.decisionsSum) {
+        printf("Empate no resultado geral!");
     }
 
     return 0;
